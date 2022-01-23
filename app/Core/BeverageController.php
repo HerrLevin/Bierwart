4f0761e19cddf810dc45ed1ff7fd0c524287f2d4
Levin Herr <github@17d.me>
2022-01-23 15:43:02 +0100

    Add createBeverageMovement to BeverageController

--- app/Core/BeverageController.php
+++ app/Core/BeverageController.php
@@ -3,18 +3,31 @@
 namespace App\Core;
 
 use App\Scaffolding\DB;
+use App\Scaffolding\Request;
+use App\Scaffolding\Response;
 
 class BeverageController
 {
-    public static function getDrinksOverview() {
+    public static function getDrinksOverview():void {
         $db = new DB();
         $db->query("SELECT bev.id, bev.name, bev.size, type.price FROM beverage bev INNER JOIN drink_type type ON bev.id_drink_type = type.id");
-        return $db->get();
+        Response::json(data: $db->get());
     }
 
-    public static function createBeverageMovement() {
+    public static function createBeverageMovement($args): void
+    {
+        $request = new Request();
         $db = new DB();
-        $db->query("SELECT bev.id, bev.name, bev.size, type.price FROM beverage bev INNER JOIN drink_type type ON bev.id_drink_type = type.id");
-        return $db->get();
+        $db->query("
+            INSERT INTO 
+            beverage_movement 
+                (id_user,id_beverage,quantity) 
+            VALUES 
+                   (".$request->bodyParam(name: 'uid').", 
+                    ".$request->bodyParam(name: 'bid').",
+                    ".$request->bodyParam(name: 'qty').");"
+        );
+        $db->execute();
+        Response::status(201);
     }
 }
