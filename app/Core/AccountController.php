bdaac82ebf98a755c37765322d2fdb334b251cae
Levin Herr <github@17d.me>
2022-02-12 11:58:28 +0100

    renaming bullshit

--- app/Core/AccountController.php
+++ app/Core/AccountController.php
@@ -10,13 +10,13 @@ class AccountController
     public static function getBalances()
     {
         $result = DB::table("user")
-            ->select(['SUM(bm.quantity*dt.price) as haben', ' soll', ' soll-SUM(bm.quantity*dt.price) as ist', ' user.id_account'])
+            ->select(['SUM(bm.quantity*dt.price) as konsum', ' konto', ' konto-SUM(bm.quantity*dt.price) as ist', ' user.id_account'])
             ->innerJoin(table: 'beverage_movement bm', first: 'user.id', operator:'=', second:'bm.id_user')
             ->innerJoin(table: 'beverage', first: 'bm.id_beverage', operator: '=', second: 'beverage.id')
             ->innerJoin(table: 'drink_type dt', first: 'beverage.id_drink_type', operator: '=', second: 'dt.id')
             ->innerJoin(
                 table: DB::table(name: "account_movement am")
-                    ->select(columns: ['SUM(amount) as soll', 'am.id_account'])
+                    ->select(columns: ['SUM(amount) as konto', 'am.id_account'])
                     ->groupBy(group: 'am.id_account')
                     ->as('test')
                     ->query(),
