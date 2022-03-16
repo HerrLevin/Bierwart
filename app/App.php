18b184db70e57c322be2e7319ce6a78b07be1c23
Levin herr <github@17d.me>
2022-03-16 13:37:50 +0100

    add origin-header + server in swagger

--- app/App.php
+++ app/App.php
@@ -5,6 +5,8 @@ namespace App;
 
 /**
  * @OA\Info(title="Bierwart API", version="0.1")
+ *
+ * @OA\Server(url="http://localhost:8000/")
  * @OA\SecurityScheme(
  *     type="http",
  *     scheme="basic",
--- app/Scaffolding/Response.php
+++ app/Scaffolding/Response.php
@@ -6,17 +6,20 @@ class Response
 {
     public static function json(array|string|null $data, int $status=200) {
         http_response_code($status);
+        header('Access-Control-Allow-Origin: *');
         echo json_encode(["data" => $data], JSON_PARTIAL_OUTPUT_ON_ERROR);
         Helpers::dd();
     }
 
     public static function status($status=200) {
         http_response_code($status);
+        header('Access-Control-Allow-Origin: *');
         Helpers::dd();
     }
 
     public static function error($message="Resource not found", $status=404) {
         http_response_code($status);
+        header('Access-Control-Allow-Origin: *');
         echo json_encode(["error" => ["message" => $message, "status" => $status]], JSON_PARTIAL_OUTPUT_ON_ERROR);
         Helpers::dd();
     }
